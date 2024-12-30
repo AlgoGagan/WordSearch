2165475d15fc61b2aaa9dbe960d1f7cebd9e4aca
AlgoGagan <gagan@algovation.in>
2024-12-30 17:14:26 +0530

    Open flower dialog via onclick of collected flowers

--- Assets/Game/WordPuzzle/Script/CollectionManager.cs
+++ Assets/Game/WordPuzzle/Script/CollectionManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ffdd02e7d6ddb164a845ddf930deb61586aeed0a6f36021cde9edba99d78d81
-size 1820
+oid sha256:66b2da7e72bfff8d488f7ce00d2bf56ff357461fca604196fce2ddb4968ac361
+size 4697
--- Assets/Game/WordPuzzle/Script/PageSlider.cs
+++ Assets/Game/WordPuzzle/Script/PageSlider.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1fa3d93db9f1481ba0b7c1892a4d33460c2f166e90cdf498ef432365b9009af
+oid sha256:ab2d871986b21e198cec922ddf5137a8990b6bbd2795c8fd563e3c19319e6d8d
 size 1807
