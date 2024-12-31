ac8feccc671e108685e2aaf53736dfa3286718a2
AlgoGagan <gagan@algovation.in>
2024-12-31 14:31:34 +0530

    Shop panel and home screen bottom navigation

--- Assets/Game/WordPuzzle/Script/Manager/EventManager.cs
+++ Assets/Game/WordPuzzle/Script/Manager/EventManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a682695e40005f7218b2197f78cc7d982022062df8639c89bfdcf541ef987a4
-size 8586
+oid sha256:005325ae2839ae7f2190f1fdad67d7c213bea74352df88c6bd1182228868ff4e
+size 10829
--- Assets/Game/WordPuzzle/Script/UIManager.cs
+++ Assets/Game/WordPuzzle/Script/UIManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5812d84381b913a8c69bcc1fdc0c17c27edbe43f9325c308b95a6bff98fd3177
-size 58758
+oid sha256:cd7d2894381bd48c51dd76e13e391b600ba222aec50a4953f788d88b413df3af
+size 58747
